ca60d5349dafa0d813c696895b74c3dc9469d664
abuzadik <abokor.saadik@gmail.com>
2024-01-23 13:07:34 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,95 @@
+<?php include('db_connect.php') ?>
+<?php
+$twhere ="";
+if($_SESSION['login_type'] != 1)
+  $twhere = "  ";
+?>
+<!-- Info boxes -->
+<?php if($_SESSION['login_type'] == 2): ?>
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM department_list ")->num_rows; ?></h3>
+
+                <p>Total Departments</p>
+              </div>
+              <div class="icon">
+                <!-- <i class="fa fa-th-list"></i> -->
+                <i class="fa fa-list-ol" style="color: #D04848;"></i>
+
+              </div>
+            </div>
+          </div>
+           <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM designation_list")->num_rows; ?></h3>
+
+                <p>Total Designations</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-id-card" style="color: #3652AD;"></i>
+              </div>
+            </div>
+          </div>
+           <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM users")->num_rows; ?></h3>
+
+                <p>Total Users</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-users" style="color: #294B29;"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM employee_list")->num_rows; ?></h3>
+
+                <p>Total Employees</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-user" style="color: #424769;"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM evaluator_list")->num_rows; ?></h3>
+
+                <p>Total Evaluators</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-check" style="color: #3C0753;"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM task_list")->num_rows; ?></h3>
+
+                <p>Total Tasks</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-tasks" style="color: #4942E4;"></i>
+              </div>
+            </div>
+          </div>
+      </div>
+
+<?php else: ?>
+   <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              Welcome <?php echo $_SESSION['login_name'] ?>!
+            </div>
+          </div>
+      </div>
+          
+<?php endif; ?>
